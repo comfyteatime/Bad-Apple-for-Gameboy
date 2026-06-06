a7ed0c62543de6145bd72c80a20442d0b8e03220
comfyteatime <comfyteatime@protonmail.com>
2026-06-06 22:34:57 +0200

    Delete Source/Shell directory

--- Source/Shell/image-resizer.sh
+++
@@ -1,9 +0,0 @@
-
-echo "How many frames to convert?"
-
-read frames
-
-for i in $(seq 1 $frames)
-do
-    convert "./bad-apple-frames/out${i}.png" -resize 40x36! ./bad-apple-gb-quality/${i}.pgm
-done
--- Source/Shell/mass-tilemap-generator.sh
+++
@@ -1,9 +0,0 @@
-
-echo "How many frames to convert?"
-
-read frames
-
-for i in $(seq 1 $frames)
-do
-    ./tilemap-generator-no-outbound.sh tileset.bin ./bad-apple-30fps-pgm/${i}.pgm ./bad-apple-30fps-frames/frame${i}.bin
-done
